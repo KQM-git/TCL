701bb0195ca2e8245422ba6019a60f9501e9b82c
Tibowl <Tibowl@users.noreply.github.com>
2022-09-26 20:38:08 +0200

    Disable trailing slashes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,6 +14,7 @@ const config = {
   url: 'https://library.keqingmains.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
+  trailingSlash: false,
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
