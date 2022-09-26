bfc9902bb3e893805bb3b9c5385cfee3287981bf
Tibowl <Tibowl@users.noreply.github.com>
2022-09-26 20:24:49 +0200

    Add disclaimer on hover

--- src/components/common/VersionHl.tsx
+++ src/components/common/VersionHl.tsx
@@ -5,9 +5,9 @@ export default function Version({ date }: { date: string }) {
   const months = (Date.now() - new Date(date).getTime()) / (30 * 24 * 3600 * 1000)
 
   if (months > 12)
-    return <span className='version-hl-error'>{date} (v. {version(date)})</span>
+    return <span className='version-hl-error' title="This hasn't been tested in over a year and might be outdated">{date} (v. {version(date)})</span>
   if (months > 6)
-    return <span className='version-hl-warning'>{date} (v. {version(date)})</span>
+    return <span className='version-hl-warning' title="This hasn't been tested in over 6 months and might be outdated">{date} (v. {version(date)})</span>
 
   return <>{date} (v. {version(date)})</>
 }
