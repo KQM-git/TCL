6f26eb03f8c1f299602ba6e4427f832c61ea34cd
Sharath <62636355+sharaths21312@users.noreply.github.com>
2025-04-17 01:47:27 +0530

    Use ESM - fix compilation on node >18

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,12 +1,12 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const {themes} = require('prism-react-renderer');
+import math from "remark-math";
+import katex from "rehype-katex";
+import { themes } from "prism-react-renderer";
+
 const lightCodeTheme = themes.github;
 const darkCodeTheme = themes.dracula;
-
-const math = require('remark-math');
-const katex = require('rehype-katex');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
