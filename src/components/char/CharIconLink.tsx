5fc98a52941c6a2e8733f40a141347bb2e4634b3
sharaths21312 <62636355+sharaths21312@users.noreply.github.com>
2025-07-30 13:22:27 +0530

    fix error for real hopefully

--- src/components/char/CharIconLink.tsx
+++ src/components/char/CharIconLink.tsx
@@ -4,7 +4,7 @@ import charlinks from '@site/src/data/char_links.json'
 
 export default function CharIconLink({ char }: { char: string }) {
   const charNameParsed = char
-    .replace(" ", "_").replace(/\((.+)\)/g, "$1").toLowerCase()
+    .replace(" ", "-").replace(/\((.+)\)/g, "$1").toLowerCase()
   console.log(charNameParsed, charlinks[charNameParsed])
   return <a href={charlinks[charNameParsed].url} style={({ textAlign: 'center' })}>
     <div><img src={`/img/characters/icon/${filename(char.replace(/Traveler \((Anemo|Cryo|Dendro|Electro|Geo|Hydro|Pyro)\)/g, "Lumine"))}.png`} width={64} height={64} /></div>
