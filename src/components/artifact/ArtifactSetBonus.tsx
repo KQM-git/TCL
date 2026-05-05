b09af693e81bf0605c5f2c0994c4524cb34a0720
sharaths21312 <62636355+sharaths21312@users.noreply.github.com>
2026-05-05 13:30:45 +0530

    Artifact formatting change

--- src/components/artifact/ArtifactSetBonus.tsx
+++ src/components/artifact/ArtifactSetBonus.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 import Admonition from '@theme/Admonition'
 
-import artifacts from '@site/src/data/artifacts.json'
 import { Artifact } from '@site/src/data/types'
 import filename from '@site/src/utils/filename'
+import artifactsData from '@site/src/data/artifacts.json'
+const artifacts = artifactsData as { [key: string]: Artifact }
 
 export default function ArtifactSetBonus({ artifact }: { artifact: string }) {
   const arti = artifacts[artifact] as Artifact
@@ -18,7 +19,7 @@ export default function ArtifactSetBonus({ artifact }: { artifact: string }) {
     <img className="icon-display-box" src={`/img/artifacts/icon/${filename(artifact)}.png`} width={256} height={256} />
     <ul>
       {arti.bonuses.map((b) => <li key={b.count}>
-        {b.count}-Piece Set Bonus: {b.desc}
+        <strong>{b.count}pc set bonus:</strong> {b.desc}
       </li>)}
     </ul>
     <div className="clear" />
